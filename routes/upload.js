d2f7c5c020968353f6b3f3e5e5cf2c8b7550b896
eathonq <vangagh@live.cn>
2022-06-29 14:23:41 +0800

    post 大小和数量限制修改

--- routes/upload.js
+++ routes/upload.js
@@ -3,7 +3,13 @@ let fs = require("fs");
 let router = express.Router();
 
 const multer = require('multer');
-const multerUpload = multer({ dest: './public/upload/' });  // multer上传文件目录配置（从根目录开始算）
+const multerUpload = multer({
+    dest: './public/upload/',   // multer上传文件目录配置（从根目录开始算）
+    limits: {
+        fileSize: 1024 * 1024 * 30, // 文件大小限制 30M
+        files: 100, // 文件上传数量限制
+    }
+});
 
 const code = require('../model/code');
 
@@ -29,7 +35,7 @@ router.post('/file', multerUpload.single('file'), function (req, res, next) {
 });
 
 // 多文件上传
-router.post('/files', multerUpload.array('file', 10), function (req, res, next) {
+router.post('/files', multerUpload.array('files'), function (req, res, next) {
     if (req.files.length === 0) {
         next({ code: code.FILE_EMPTY });
         return;
@@ -46,10 +52,12 @@ router.post('/files', multerUpload.array('file', 10), function (req, res, next) 
         }
 
         let _files = [];
+        console.log(`count: ${req.files.length}`);
         for (let i = 0; i < req.files.length; i++) {
             let file = req.files[i];
-            console.log(file);
-            fs.renameSync('./public/upload/' + file.filename, path + '/' + file.originalname);  // 可以修改文件名称
+            //console.log(file);
+            console.log(`originalname: ${file.originalname}, size: ${file.size}, path: ${path}`);
+            fs.renameSync('./public/upload/' + file.filename, path + '/' + file.originalname);  // 可以修改文件名称(转移到指定目录)
             _files.push(file.originalname);
         }
 
